27d02cb12cf0e990c34756456b9d465ccbafbc3b
Shravan Shetty <shsh9888@colorado.edu>
2020-04-19 14:15:31 -0600

    Initial setup

---
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.eggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
